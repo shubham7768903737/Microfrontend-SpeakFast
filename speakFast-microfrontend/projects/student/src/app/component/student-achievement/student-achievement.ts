a63180924403d5bbac404e2ba5cf213c9611f6b7
YogeshBurud <yogeshburud@gmail.com>
2026-08-20 19:32:41 +0530

    updated code

---
+++ speakFast-microfrontend/projects/student/src/app/component/student-achievement/student-achievement.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-achievement',
+  imports: [],
+  templateUrl: './student-achievement.html',
+  styleUrl: './student-achievement.css',
+})
+export class StudentAchievement {}
