20a238f47445d8b71ef1b7667dda64727fcd5dc9
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2026-08-21 14:42:29 +0530

    install the angular material and create basic route (#9)

--- speakFast-microfrontend/projects/admin/src/app/app.routes.ts
+++ speakFast-microfrontend/projects/admin/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path : '',
+    loadComponent: () =>
+        import('./component/admin/admin').then((module) => module.Admin),
+   },
+   {
+    path : 'admin',
+    loadComponent: () =>
+        import('./component/admin/admin').then((module) => module.Admin),
+   }
+];
---
+++ speakFast-microfrontend/projects/admin/src/app/component/admin-teachers/admin-teachers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-teachers',
+  imports: [],
+  templateUrl: './admin-teachers.html',
+  styleUrl: './admin-teachers.css',
+})
+export class AdminTeachers {}
