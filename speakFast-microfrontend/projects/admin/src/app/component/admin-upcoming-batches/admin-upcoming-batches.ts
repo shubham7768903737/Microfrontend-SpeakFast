3085e439d96ab7b832358c648d35a4f0c5f4fc48
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2026-08-21 14:10:04 +0530

    install the angular material and create basic route

---
+++ speakFast-microfrontend/projects/admin/src/app/component/admin-upcoming-batches/admin-upcoming-batches.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-upcoming-batches',
+  imports: [],
+  templateUrl: './admin-upcoming-batches.html',
+  styleUrl: './admin-upcoming-batches.css',
+})
+export class AdminUpcomingBatches {}
