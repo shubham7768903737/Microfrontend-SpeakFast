92c5308bd4c518a259fd1c18bf4c9cef916f6c54
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2026-08-23 19:16:44 +0530

    change the admin folder structure and add the packages

--- speakFast-microfrontend/projects/admin/src/app/core/Services/alert.service.ts
+++ speakFast-microfrontend/projects/admin/src/app/core/Services/alert.service.ts
@@ -1,8 +1,149 @@
+import { ToastrService } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 
+import Swal, {
+  SweetAlertIcon,
+  SweetAlertResult
+} from 'sweetalert2';
+
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AlertService {
-  
+
+  constructor(private toster: ToastrService){}
+
+
+  // ==============================
+  // Confirm Alert
+  // ==============================
+  confirm(
+    title: string,
+    html: string,
+    icon: SweetAlertIcon = 'warning',
+    confirmButtonText: string = 'Yes',
+    cancelButtonText: string = 'Cancel'
+  ): Promise<SweetAlertResult<any>> {
+
+    return Swal.fire({
+
+      title: title,
+
+      // IMPORTANT:
+      // text ऐवजी html
+      html: html,
+
+      icon: icon,
+
+      showCancelButton: true,
+
+      confirmButtonText: confirmButtonText,
+      cancelButtonText: cancelButtonText,
+
+      reverseButtons: false,
+
+      heightAuto: false,
+      returnFocus: false,
+      scrollbarPadding: false,
+
+      buttonsStyling: false,
+
+      customClass: {
+        popup: 'custom-delete-popup',
+        icon: 'custom-delete-icon',
+        title: 'custom-delete-title',
+        htmlContainer: 'custom-delete-text',
+        actions: 'custom-delete-actions',
+        confirmButton: 'custom-confirm-btn',
+        cancelButton: 'custom-cancel-btn'
+      }
+
+    });
+
+  }
+
+
+  // ==============================
+  // Success Popup
+  // ==============================
+  success(title: string, text: string) {
+
+    return Swal.fire({
+
+      title,
+      text,
+
+      icon: 'success',
+
+      buttonsStyling: false,
+
+      customClass: {
+        popup: 'custom-popup',
+        confirmButton: 'custom-confirm-btn'
+      }
+
+    });
+
+  }
+
+
+  // ==============================
+  // Error Popup
+  // ==============================
+  error(title: string, text: string) {
+
+    return Swal.fire({
+
+      title,
+      text,
+
+      icon: 'error',
+
+      buttonsStyling: false,
+
+      customClass: {
+        popup: 'custom-popup',
+        confirmButton: 'custom-confirm-btn'
+      }
+
+    });
+
+  }
+
+
+  // ==============================
+  // Info Popup
+  // ==============================
+  info(title: string, text: string) {
+
+    return Swal.fire({
+
+      title,
+      text,
+
+      icon: 'info',
+
+      buttonsStyling: false,
+
+      customClass: {
+        popup: 'custom-popup',
+        confirmButton: 'custom-confirm-btn'
+      }
+
+    });
+
+  }
+
+  toasterSuccess(message: string){
+    this.toster.success(message)
+  }
+
+  tosterUnsuccess(message: string){
+    this.toster.error(message)
+  }
+
+  tosterInfo(message: string){
+    this.toster.info(message)
+  }
+
 }
--- speakFast-microfrontend/projects/shell/src/app/app.routes.ts
+++ speakFast-microfrontend/projects/shell/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { loadRemoteModule } from '@angular-architects/module-federation';
 export const routes: Routes = [
 	{
 		path: 'admin',
-		loadComponent: () =>
+		loadChildren: () =>
 			loadRemoteModule({
 				type: 'module',
 				remoteEntry: 'http://localhost:4201/remoteEntry.js',
