c2f35cc9f5440dba28536db97e865a2b777bfafe
shubham7768903737 <sparit3737@gmail.com>
2026-08-24 15:35:54 +0530

    add login chnages

--- speakFast-microfrontend/projects/shell/src/app/app.routes.ts
+++ speakFast-microfrontend/projects/shell/src/app/app.routes.ts
@@ -7,7 +7,45 @@ export const routes: Routes = [
 		loadComponent: () =>
 			import('./componets/login/login').then((module) => module.Login),
 	},
-	{ path: 'login/confirmPassword', loadComponent: () => import('./componets/forgot-password/confirm-password/confirm-password').then((module) => module.ConfirmPassword) },
+	{
+		path : "forgotPassword", loadComponent : ()=> import('./componets/forgot-password/forgot-password').then((m)=>m.ForgotPassword),
+
+		children :[
+			{
+        path: '',
+        redirectTo: 'resetpassword',
+        pathMatch: 'full'
+      },
+
+      {
+        path: 'resetpassword',
+        loadComponent: () =>
+          import('./componets/forgot-password/reset-password/reset-password')
+            .then(m => m.ResetPassword)
+      },
+
+      {
+        path: 'sentLink',
+        loadComponent: () =>
+          import('./componets/forgot-password/link-sent/link-sent')
+            .then(m => m.LinkSent)
+      },
+
+      {
+        path: 'confirmPassword/:token',
+        loadComponent: () =>
+          import('./componets/forgot-password/confirm-password/confirm-password')
+            .then(m => m.ConfirmPassword)
+      },
+
+      {
+        path: 'passwordChanged',
+        loadComponent: () =>
+          import('./componets/forgot-password/password-changed/password-changed')
+            .then(m => m.PasswordChanged)
+      }
+		]
+	},
 	{
 		path: 'admin',
 		loadChildren: () =>
--- speakFast-microfrontend/projects/student-registration/src/app/component/teachers-slots/teachers-slots.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-teachers-slots',
-  imports: [],
-  templateUrl: './teachers-slots.html',
-  styleUrl: './teachers-slots.css',
-})
-export class TeachersSlots {}
