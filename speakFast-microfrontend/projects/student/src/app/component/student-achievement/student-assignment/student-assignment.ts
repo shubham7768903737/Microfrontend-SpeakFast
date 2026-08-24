18842a32e2834f0b911a0cc2d18e25b619bff662
Harshal <patilharshal0362@gmail.com>
2026-08-24 12:38:46 +0530

    Latest Pull

---
+++ speakFast-microfrontend/projects/student/src/app/component/student-achievement/student-assignment/student-assignment.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-assignment',
+  imports: [],
+  templateUrl: './student-assignment.html',
+  styleUrl: './student-assignment.css',
+})
+export class StudentAssignment {}
