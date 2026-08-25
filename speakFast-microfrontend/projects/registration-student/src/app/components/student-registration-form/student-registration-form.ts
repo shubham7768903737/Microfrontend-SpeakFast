57e54ab94a45dc4d8f0dc2ff02cd9fcad4216565
yerudkarsourabh <yerudkarsourabh9@gmail.com>
2026-08-25 12:36:45 +0530

    creating componts

---
+++ speakFast-microfrontend/projects/registration-student/src/app/components/student-registration-form/student-registration-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-registration-form',
+  imports: [],
+  templateUrl: './student-registration-form.html',
+  styleUrl: './student-registration-form.css',
+})
+export class StudentRegistrationForm {}
