906340bb13d3cecafd39d88aabf2d3940256398c
Pruthviraj Dadhaniya <pruthavirajdadhaniya@gmail.com>
2026-08-27 11:31:43 +0530

    latest code

--- speakFast-microfrontend/projects/admin/src/app/component/admin/admin-teachers/admin-teachers.spec.ts
+++ speakFast-microfrontend/projects/admin/src/app/component/admin/admin-teachers/admin-teachers.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminTeachers } from './admin-teachers';
 
+declare function describe(description: string, specDefinitions: () => void): void;
+declare function beforeEach(action: () => void | Promise<void>): void;
+declare function it(description: string, testFunction: () => void | Promise<void>): void;
+declare function expect(actual: unknown): { toBeTruthy(): void };
+
 describe('AdminTeachers', () => {
   let component: AdminTeachers;
   let fixture: ComponentFixture<AdminTeachers>;
